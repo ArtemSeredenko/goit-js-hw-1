2a7aec3fe5449e03ba736afd3a5a13da292dafa4
ArtemSeredenko <mpdart2013@gmail.com>
2024-04-16 13:46:09 +0300

    del fetch add axios and load more

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,21 +3,18 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '36191554-a4c97c0aee7312389854400e2';
 
-function getData(str) {
+async function getData(str, page) {
   const parameters = new URLSearchParams({
     key: API_KEY,
     q: str,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-    per_page: 32,
+    page,
+    per_page: 15,
   });
 
-  return fetch(`${BASE_URL}?${parameters}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-    return response.json();
-  });
+  const response = await axios(`${BASE_URL}?${parameters}`);
+  return response.data;
 }
 export { getData };
--- src/main.js
+++ src/main.js
@@ -5,21 +5,33 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 import { getData } from './js/pixabay-api.js';
-import { createMarkUp } from './js/render-functions.js';
-import { form, input, gallery } from './js/refs.js';
+import { createMarkup } from './js/render-functions.js';
+import { form, input, gallery, loadMoreBtn } from './js/refs.js';
 import { loaderShow } from './js/loader.js';
+import { addLoadMoreBtn, removeLoadMoreBtn } from './js/load-more-btn.js';
+import { scrollScreen } from './js/scroll-screen';
 
-let searchInput = '';
+let searchQuery = '';
+
 const photosGallery = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-form.addEventListener('submit', onFormSubmit);
+let page = 1;
+let totalPages;
 
-function onFormSubmit(event) {
+form.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', loadMoreHandle);
+
+async function onFormSubmit(event) {
   event.preventDefault();
   gallery.innerHTML = '';
+  page = 1;
+
+  if (!loadMoreBtn.classList.contains('hidden')) {
+    removeLoadMoreBtn();
+  }
 
   if (input.value.trim() === '') {
     return iziToast.error({
@@ -31,28 +43,72 @@ function onFormSubmit(event) {
     });
   }
 
-  searchInput = input.value;
+  searchQuery = input.value;
   loaderShow();
 
-  getData(searchInput)
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          title: '',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-          timeout: 3000,
-          pauseOnHover: false,
-        });
-        loaderShow();
-      } else {
-        gallery.insertAdjacentHTML('beforeend', createMarkUp(data.hits));
+  try {
+    const data = await getData(searchQuery, page);
+    if (data.hits.length === 0) {
+      iziToast.error({
+        title: '',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+        timeout: 3000,
+        pauseOnHover: false,
+      });
+    } else {
+      gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
 
-        photosGallery.refresh();
-        loaderShow();
+      photosGallery.refresh();
+
+      totalPages = data.totalHits / data.hits.length;
+
+      if (page < totalPages) {
+        addLoadMoreBtn();
       }
-    })
-    .catch(error => console.log(error))
-    .finally(() => form.reset());
+    }
+  } catch (error) {
+    alert(error.message);
+  } finally {
+    form.reset();
+  }
+  loaderShow();
+}
+
+async function loadMoreHandle() {
+  page += 1;
+  removeLoadMoreBtn();
+  loaderShow();
+  try {
+    const data = await getData(searchQuery, page);
+    gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+
+    photosGallery.refresh();
+
+    const galleryItem = document.querySelector('.gallery-item');
+    scrollScreen(galleryItem);
+
+    if (page >= totalPages && data.totalHits) {
+      loaderShow();
+      removeLoadMoreBtn();
+      iziToast.info({
+        title: '',
+        message: "We're sorry, but you've reached the end of search results!",
+        position: 'bottomRight',
+        timeout: 3000,
+        pauseOnHover: false,
+      });
+    }
+    loaderShow();
+    addLoadMoreBtn();
+  } catch (error) {
+    alert(error.message);
+    removeLoadMoreBtn();
+  } finally {
+    if (page >= totalPages) {
+      removeLoadMoreBtn();
+      loaderShow();
+    }
+  }
 }
